1de21de597eb776d3cec34ad233f6f450f4c1aa4
Munir Siddiqui <munirsid@bu.edu>
2024-02-11 20:05:33 -0500

    fixed confusion matrix

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -555,10 +555,26 @@ def main():
     detected = results[1] + results[2] + results[3] + results[4] + results[5]
     print(detected)
 
-    cm = confusion_matrix(actual, detected, normalize='true')
-    sns.heatmap(cm, annot=True)
+    cm = confusion_matrix(actual, detected)
+    total_per_class = np.sum(cm, axis=1)
+    percentages = np.around((cm.astype('float') / total_per_class[:, np.newaxis]) * 100, decimals=1)
+
+    # Create labels for the heatmap
+    labels = []
+    for i in range(5):
+        for j in range(5):
+            labels.append(f'{percentages[i][j]}%\n({cm[i][j]})')
+
+    # Reshape the labels to match the shape of the confusion matrix for plotting
+    labels = np.array(labels).reshape(5, 5)
+
+    # Plot the heatmap
+    sns.heatmap(cm, annot=labels, fmt='', cmap='Blues')
+
+    # Set the x and y axis labels to range from 1 to 5
     plt.xticks(np.arange(5) + 0.5, labels=np.arange(1, 6))
     plt.yticks(np.arange(5) + 0.5, labels=np.arange(1, 6))
+
     plt.xlabel('Detected')
     plt.ylabel('Actual')
     plt.show()
